6680ecd57ba82128f3116961daa66cf299979dbc
Falcon <37326776+BlackFalcons@users.noreply.github.com>
2020-11-22 17:57:15 +0100

    Add maximum ammo to all weapons

--- Falcon's give all weapon's script/Falcon's give all weapon's script.lua
+++ Falcon's give all weapon's script/Falcon's give all weapon's script.lua
@@ -2,62 +2,62 @@
 local mafia_2_weapons = {
     Model_12_Revolver = {
         ID = 2,
-        ammo = 36
+        ammo = 42
     },
 
     Mauser_C96 = {
         ID = 3,
-        ammo = 50
+        ammo = 60
     },
 
     Colt_M1911A1 = {
         ID = 4,
-        ammo = 49
+        ammo = 56
     },
 
     Colt_M1911_Special = {
         ID = 5,
-        ammo = 69
+        ammo = 92
     },
 
     Model_19_Revolver = {
         ID = 6,
-        ammo = 36
+        ammo = 42
     },
     
     -- MK2_Dummy_Frag_Grenade = {
-    --     ID = 20,
+    --     ID = 7,
     --     ammo = 5
     -- },
 
     Remington_Model_870_Field_gun = {
         ID = 8,
-        ammo = 48
+        ammo = 56
     },
 
     M3_Grease_Gun = {
         ID = 9,
-        ammo = 90
+        ammo = 120
     },
 
     MP40 = {
         ID = 10,
-        ammo = 96
+        ammo = 128
     },
 
     Thompson_1928 = {
         ID = 11,
-        ammo = 150
+        ammo = 200
     },
 
     M1A1_Thompson = {
         ID = 12,
-        ammo = 90
+        ammo = 120
     },
 
     Beretta_Model_38A = {
         ID = 13,
-        ammo = 90
+        ammo = 120
     },
 
     -- MG42 = {
@@ -67,22 +67,22 @@ local mafia_2_weapons = {
 
     M1_Garand = {
         ID = 15 ,
-        ammo = 32
+        ammo = 40
     },
 
     Kar98k = {
         ID = 17,
-        ammo = 30
+        ammo = 35
     },
 
     MK2_Frag_Grenade = {
         ID = 20,
-        ammo = 5
+        ammo = 6
     },
 
     Molotov_Cocktail = {
         ID = 21,
-        ammo = 5
+        ammo = 6
     }
 }
 
