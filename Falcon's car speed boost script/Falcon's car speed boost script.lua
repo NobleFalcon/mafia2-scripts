b757a0910023d1e5a39d793d0ac82ea03fae97d2
Eivind <37326776+NobleFalcon@users.noreply.github.com>
2024-05-04 12:44:01 +0200

    Implement delay buffer for setCarSpeed function to prevent crashes
    Introduces a delay buffer to manage the timing of setCarSpeed function executions. By setting a minimum delay of 50 milliseconds between calls, the update avoids overwhelming the game engine, which helps prevent crashes that occurred when this function was called too frequently. The use of DelayBuffer ensures that the car's speed is updated safely and in a controlled manner.

--- Falcon's car speed boost script/Falcon's car speed boost script.lua
+++ Falcon's car speed boost script/Falcon's car speed boost script.lua
@@ -1,8 +1,13 @@
-local speedBoost = 50
+local speedBoost = 30
 
 function setCarSpeed()
     local carSpeed = game.game:GetActivePlayer():GetOwner():GetSpeedFloat() + speedBoost
     game.game:GetActivePlayer():GetOwner():SetSpeed(carSpeed)
 end
 
-DelayBuffer:Insert( setCarSpeed, _, 250, 1, false )
+-- Settings for the delay buffer
+local delayMilliseconds = 50
+local delayIterations = 1
+local delayAsync = false
+
+DelayBuffer:Insert(setCarSpeed, nil, delayMilliseconds, delayIterations, delayAsync)
